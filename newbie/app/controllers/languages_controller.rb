7724a9b88eaa7b72e5746fd5632643a5d19441f7
Chad Smith <chadxsmith@ymail.com>
2015-05-12 20:24:47 -0400

    latest

--- newbie/app/controllers/languages_controller.rb
+++ newbie/app/controllers/languages_controller.rb
@@ -3,7 +3,25 @@ class LanguagesController < ApplicationController
 
     def index
         @languages = Language.all
+        @topics = Topic.all
+        @questions = Question.all
+        @answers = Answer.all
+        @everything = []
+
+        @languages.each { |language|  @everything.push(language).to_s }
+        @topics.each { |topic|  @everything.push(topic).to_s }
+        @questions.each { |question|  @everything.push(question).to_s }
+        @answers.each { |answer|  @everything.push(answer).to_s }
+
+
+        respond_to do |format|
+
+        format.html { render :index}
+        format.json { render json: @everything}
+
+        end
     end
+
 
     def show
         @language = Language.find(params[:id])
