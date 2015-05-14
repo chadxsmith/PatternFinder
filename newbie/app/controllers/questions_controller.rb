9e734f132d890e390e1601cad565843aeb3abc58
Chad Smith <chadxsmith@ymail.com>
2015-05-14 13:06:36 -0400

    search box working

--- newbie/app/controllers/questions_controller.rb
+++ newbie/app/controllers/questions_controller.rb
@@ -2,8 +2,8 @@ class QuestionsController < ApplicationController
 
     def show
         @question = Question.find(params[:id])
-        @language = Language.find(params[:id])
-        @topic = Topic.find(params[:id])
+        @language = Language.find(params[:language_id])
+        @topic = Topic.find(params[:topic_id])
     end
 
 end
